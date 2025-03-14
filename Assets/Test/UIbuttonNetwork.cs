698dabdd537a9d15100260b832bfcf5ff6be02d8
Phuc Le <lenguyenminhphuc0706@gmail.com>
2025-03-14 22:17:11 +0700

    adjust multiplayer mode

--- Assets/Test/UIbuttonNetwork.cs
+++ Assets/Test/UIbuttonNetwork.cs
@@ -1,19 +1,67 @@
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 using Unity.Netcode;
+using TMPro;
+using System.Net;
+using System.Net.Sockets;
+using System;
+using Unity.Netcode.Transports.UTP;
 
 public class UIbuttonNetwork : MonoBehaviour
 {
+    public UnityTransport transport;
+    public TMP_Text address;
     public Button HostButton, ClientButton, ServerButton;
-    void Update()
+
+    void Start()
     {
-        HostButton.onClick.AddListener(() => { NetworkManager.Singleton.StartHost(); alertClick(); });
-        ClientButton.onClick.AddListener(() => { NetworkManager.Singleton.StartClient(); alertClick(); });
-        ServerButton.onClick.AddListener(() => { NetworkManager.Singleton.StartServer(); alertClick(); });
+        // Gán sự kiện khi game bắt đầu, tránh gán liên tục trong Update()
+        HostButton.onClick.AddListener(StartHost);
+        ClientButton.onClick.AddListener(StartClient);
+        ServerButton.onClick.AddListener(StartServer);
+
+        // Lấy IP của máy local nếu cần hiển thị
+        address.text = GetLocalIPAddress();
     }
 
-    void alertClick()
+    void StartHost()
     {
-        Debug.Log("Button Start game is clicked!");
+        transport = NetworkManager.Singleton.GetComponent<UnityTransport>();
+        transport.ConnectionData.Address = GetLocalIPAddress(); // Gán địa chỉ trước khi start
+        NetworkManager.Singleton.StartHost();
+        Debug.Log("Host started at: " + transport.ConnectionData.Address);
+    }
+
+    void StartClient()
+    {
+        if (NetworkManager.Singleton.IsClient || NetworkManager.Singleton.IsHost)
+        {
+            Debug.LogWarning("Client or Host is already running!");
+            return;
+        }
+
+        transport = NetworkManager.Singleton.GetComponent<UnityTransport>();
+        transport.ConnectionData.Address = "10.0.2.15"; // Thay đổi nếu cần kết nối IP khác
+        Debug.Log("Client connecting to: " + transport.ConnectionData.Address);
+        NetworkManager.Singleton.StartClient();
+    }
+
+    void StartServer()
+    {
+        NetworkManager.Singleton.StartServer();
+        Debug.Log("Server started.");
+    }
+
+    string GetLocalIPAddress()
+    {
+        var host = Dns.GetHostEntry(Dns.GetHostName());
+        foreach (var ip in host.AddressList)
+        {
+            if (ip.AddressFamily == AddressFamily.InterNetwork)
+            {
+                return ip.ToString();
+            }
+        }
+        throw new Exception("No network adapters with an IPv4 address in the system!");
     }
 }
